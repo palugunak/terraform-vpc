d807d716b72e6f62adfa4162801369997477bf49
[palugunak] <[palugunak@gmail.com]>
2024-04-15 10:23:17 -0400

    added variable.tf and other files

---
+++ datasources.tf
@@ -0,0 +1,11 @@
+data "aws_ami" "ami" {
+
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20240228"]
+  }
+
+}
